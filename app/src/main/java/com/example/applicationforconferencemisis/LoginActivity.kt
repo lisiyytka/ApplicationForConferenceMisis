6773eef575c5c8a899179f57313ede299cb0db45
Slava Shumakov <shum.slav@yandex.ru>
2021-05-21 01:30:04 +0500

    Models and SQLiteHelper

--- app/src/main/java/com/example/applicationforconferencemisis/LoginActivity.kt
+++ app/src/main/java/com/example/applicationforconferencemisis/LoginActivity.kt
@@ -15,6 +15,7 @@ import com.example.applicationforconferencemisis.Data.SQLite.SQLiteHelper
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        initFirebase()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         val helper = SQLiteHelper(this)
@@ -28,49 +29,33 @@ class LoginActivity : AppCompatActivity() {
         buttonForLogin.setOnClickListener {
             val email = editTextEmail.text.toString()
             val pswrd = editTextPassword.text.toString()
-            when (findUser(email, pswrd)) {
-                USERFIND -> startActivity(Intent(this, MainActivity::class.java))
-                WRONGPSWD -> makeToast(this, "Wrong password")
-                INCORRECTDATA -> makeToast(this, "Fill in the fields!")
-                USERISNOTFIND -> {
-                    helper.deleteUser()
-                    val user = User(email,"","",pswrd)
-                    helper.insertUser(user)
-                    addNewUser(user)
-                    startActivity(Intent(this, MainActivity::class.java))
-                }
-            }
-        }
-    }
-
-    fun findUser(emailEntrance: String, pswdEntrance: String): StatusLogin {
-        if (emailEntrance.isEmpty() || pswdEntrance.isEmpty()) {
-            return INCORRECTDATA
-        }
-        initFirebase()
-        val ref = REF_DATABASE_ROOT.child(NODE_USERS)
-        var result = USERISNOTFIND
-        val helper = SQLiteHelper(this)
-        ref.addListenerForSingleValueEvent(
-            AppValueEventListener {
-                if (it.hasChild(emailEntrance)) {
-                    REF_DATABASE_ROOT.child(NODE_USERS).child(emailEntrance).addListenerForSingleValueEvent(
-                        AppValueEventListener {
-                            val user = it.getValue(User::class.java)
-                            if (user!!.password == pswdEntrance) {
-                                result = USERFIND
+            if (email.isEmpty() || pswrd.isEmpty())
+                makeToast(this, "Fill all the fields")
+            else {
+                val ref = REF_DATABASE_ROOT.child(NODE_USERS)
+                ref.addListenerForSingleValueEvent(
+                    AppValueEventListener {
+                        if (it.hasChild(email)) {
+                            val user = it.child(email).getValue(User::class.java)
+                            if (user!!.password == pswrd) {
                                 helper.deleteUser()
                                 helper.insertUser(user)
+                                startActivity(Intent(this, MainActivity::class.java))
+                            } else {
+                                makeToast(this, "WrongPswrd")
                             }
-                            else
-                                result = WRONGPSWD
-                            //Я не знаю почему он вместо того чтобы вывести мне неправильный пароль просто заменяет его
+                        } else {
+                            makeToast(this, "NotFindMakeNew")
+                            val user = User(email, "", "", pswrd)
+                            addNewUser(user)
+                            helper.deleteUser()
+                            helper.insertUser(user)
+                            startActivity(Intent(this, MainActivity::class.java))
                         }
-                    )
-                }
+                    }
+                )
             }
-        )
-        return result
+        }
     }
 }
 
