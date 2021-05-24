95c57df50233d646414bc01d14997fafae568313
komle <k_nastya3@mail.ru>
2021-05-24 18:23:02 +0500

    messages

--- app/src/main/java/com/example/applicationforconferencemisis/Fragments/AccountFragment.kt
+++ app/src/main/java/com/example/applicationforconferencemisis/Fragments/AccountFragment.kt
@@ -8,12 +8,16 @@ import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.applicationforconferencemisis.Activities.LoginActivity
 import com.example.applicationforconferencemisis.Activities.MainActivity
 import com.example.applicationforconferencemisis.Data.SQLite.SQLiteHelper
 import com.example.applicationforconferencemisis.R
 
 class AccountFragment: Fragment() {
+
+    private val registerFragment = RegisterFragment()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -25,11 +29,18 @@ class AccountFragment: Fragment() {
     override fun onResume() {
         super.onResume()
         val logout = view!!.findViewById<ImageView>(R.id.logout)
+        val edit = view!!.findViewById<ImageView>(R.id.edit)
         val helper = SQLiteHelper(context!!)
 
         logout.setOnClickListener {
             helper.deleteAllPersonalData()
             startActivity(Intent(context!!, LoginActivity::class.java))
+        }
+
+        edit.setOnClickListener {
+            val fragmentManager = childFragmentManager.beginTransaction()
+            fragmentManager.replace(R.id.containerForFrag, registerFragment)
+            fragmentManager.commit()
         }
     }
 }
